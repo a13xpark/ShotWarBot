b5ef4c8dbe66bc3790308160430cf943b5689d59
a1ex <ilzavfxx@gmail.com>
2026-05-20 20:15:15 +0100

    Update Cooldown.js

--- models/Cooldown.js
+++ models/Cooldown.js
@@ -1,1 +1,22 @@
+const mongoose =
+require("mongoose");
 
+const schema =
+new mongoose.Schema({
+
+factionId:String,
+
+endsAt:Date,
+
+inWar:{
+type:Boolean,
+default:false
+}
+
+});
+
+module.exports =
+mongoose.model(
+"Cooldown",
+schema
+);
